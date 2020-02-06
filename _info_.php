e769a7debc88902788d35772f59ef5b9c5e65bec
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-02-06 15:42:30 +0100

    Update _info_.php

--- _info_.php
+++ _info_.php
@@ -1,12 +1,13 @@
 <?
 $mod_name="karma";
-$mod_version="1.7";
+$mod_version="2.b";
 $mod_path="/usr/share/fruitywifi/www/modules/$mod_name";
 $mod_logs="$log_path/$mod_name.log"; 
 $mod_logs_history="$mod_path/includes/logs/";
 $mod_logs_panel="enabled";
 $mod_panel="show";
 $mod_type="service";
+$mod_isup="$bin_sudo $mod_path/includes/hostapd_cli -p /var/run/hostapd karma_get_state | grep 'ENABLE'";
 $mod_alias="Karma";
 
 # EXEC
@@ -23,9 +24,4 @@ $bin_sed = "/bin/sed";
 $bin_rm = "/bin/rm";
 $bin_route = "/sbin/route";
 $bin_perl = "/usr/bin/perl";
-
-# ISUP
-//$mod_isup="/usr/share/FruityWifi/www/modules/karma/includes/hostapd_cli -p /var/run/hostapd-phy0 karma_get_state | tail -1 | grep EN";
-//$mod_isup="$bin_danger \"$mod_path/includes/hostapd_cli -p /var/run/hostapd-phy0 karma_get_state | grep 'ENABLE'\"";
-$mod_isup="$bin_sudo $mod_path/includes/hostapd_cli -p /var/run/hostapd karma_get_state | grep 'ENABLE'";
 ?>
